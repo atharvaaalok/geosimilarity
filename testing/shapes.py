cd11ff12039ca3eb73149f189f63d8eb49d099e4
atharvaaalok <atharvaaalok@gmail.com>
2025-02-07 10:38:03 -0800

    Add shape generation functions.
    Add shape generation functions for unit circle and square.

---
+++ testing/shapes.py
@@ -0,0 +1,27 @@
+import torch
+
+
+def circle(num_pts: int) -> torch.Tensor:
+    theta = torch.linspace(0, 2 * torch.pi, num_pts)
+    X = torch.stack([torch.cos(theta), torch.sin(theta)], dim = 1)
+    return X
+
+
+def square(num_pts: int) -> torch.Tensor:
+    # Generate points on the unit circle first and then map them to a square
+    theta = torch.linspace(0, 2 * torch.pi, num_pts)
+
+    x, y = torch.cos(theta), torch.sin(theta)
+    s = torch.maximum(torch.abs(x), torch.abs(y))
+    x_sq, y_sq = x/s, y/s
+
+    X = torch.stack([x_sq, y_sq], dim = 1)
+    return X
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    X = square(num_pts = 100)
+    plt.plot(X[:, 0], X[:, 1])
+    plt.show()
