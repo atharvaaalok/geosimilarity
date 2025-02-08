25215494fc1189ba00f62696374eba61d9e76d57
atharvaaalok <atharvaaalok@gmail.com>
2025-02-07 21:12:04 -0800

    Add SmoothHausdorff Loss.

--- geosimilarity/__init__.py
+++ geosimilarity/__init__.py
@@ -4,3 +4,4 @@ from .max_abs_error import MaxAbsErrorLoss
 from .max_sq_error import MaxSquaredErrorLoss
 from .smooth_max_sq_error import SmoothMaxSquaredErrorLoss
 from .hausdorff import HausdorffLoss
+from .smooth_hausdorff import SmoothHausdorffLoss
