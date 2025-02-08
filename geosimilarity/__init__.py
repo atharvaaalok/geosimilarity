0eb6c81fb155e13f03d5e740a2d8a2764c97a209
atharvaaalok <atharvaaalok@gmail.com>
2025-02-07 20:18:34 -0800

    Add SmoothMaxSquaredError Loss.

--- geosimilarity/__init__.py
+++ geosimilarity/__init__.py
@@ -2,3 +2,4 @@ from .mse import MSELoss
 from .mae import MAELoss
 from .max_abs_error import MaxAbsErrorLoss
 from .max_sq_error import MaxSquaredErrorLoss
+from .smooth_max_sq_error import SmoothMaxSquaredErrorLoss
